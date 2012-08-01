2cf824f75c879d8d5b9015f5efed7ad568c2eb6c
Stefan Koolen <kaas.koolen@gmail.com>
2012-08-01 14:37:04 +0200

    Added Printf program

---
+++ Printf.c
@@ -0,0 +1,14 @@
+/************************************************
+* Made by Stefan Koolen			 	*
+* My GitHub: http://www.github.com/StefanKoolen *
+************************************************/
+#include<stdio.h>
+
+	main(){ /*Start of program*/
+
+		printf("This is my first C program");
+		printf("that will be placed on GitHub!\n");
+		printf("I will compile every C program first before I will push it to GitHub!\n");
+
+
+	}	/*End of program*/
