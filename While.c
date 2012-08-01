31758a877030c1f874bc1ef025c74b4bc22f21d9
Stefan Koolen <kaas.koolen@gmail.com>
2012-08-01 15:59:55 +0200

    Added While program

---
+++ While.c
@@ -0,0 +1,24 @@
+/************************************************
+* Made by Stefan Koolen			 	*
+* My GitHub: http://www.github.com/StefanKoolen *
+************************************************/
+#include<stdio.h>
+
+	main(){ /*Use the while loop*/
+
+int i;
+i = 0;
+
+		while(i<=10){
+
+			printf("I am using the while loop!\n");
+			i = i++;
+
+
+		}
+
+
+
+
+
+	}
